0b892eaf83fc8d7ea7b5aaf6d9f296fb52d96c83
Govind Sahai <gosahai@amazon.com>
2024-04-02 11:49:32 +0530

    Revert "Added test2 site"
    This reverts commit f75b392bce20ab615e7e5f9061d9214ca29a3cc1.

--- test2/otp_files/acs.js
+++
@@ -1,8 +0,0 @@
-antjq(document).ready(function () {
-    // Do not show current langauge in language selector
-    antjq('.dropdown-content a').each(function () {
-        if (antjq(this).data('lang-label') == antjq('#language-selector button').text()) {
-            antjq(this).remove();
-        }
-    });
-});
